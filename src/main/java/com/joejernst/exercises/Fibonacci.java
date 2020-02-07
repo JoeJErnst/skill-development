b4c6e794beec4e839d2b3f3bb12cc20dfbf118e2
Joe J Ernst <joe.ernst@autoap.com>
2020-02-06 22:57:20 -0800

    added all files

--- src/main/java/com/joejernst/exercises/Fibonacci.java
+++ src/main/java/com/joejernst/exercises/Fibonacci.java
@@ -1,4 +1,41 @@
 package com.joejernst.exercises;
 
+/**
+ * Methods for traversing a Fibonacci sequence
+ *
+ * @author JoeJErnst
+ */
 public class Fibonacci {
+
+    /**
+     * Returns the nth number (zero-based) in a Fibonacci sequence beginning with 0,1.  Remember, each number a Fibonacci sequence is the
+     * sum of the two previous numbers.
+     *
+     * Fibonacci sequence: 0,1,1,2,3,5,8,13...
+     *                 n = 0,1,2,3,4,5,6,7...
+     *
+     * @param n The position you want returned
+     * @return The int value contained in the nth position of the sequence
+     */
+    static long findNthNumber(int n) {
+        if(n < 0) throw new IllegalArgumentException("You must provide a positive integer");
+
+        // Return immediately for first two values because n = result for 0 and 1
+        if (n <= 1) return n;
+
+        long a = 0, b = 1, c;
+
+        for(int i = 2; i <= n; i++) {
+            c = a+b; // calculate the value for this position
+
+            // Integer overflow
+            if(c < 0) throw new IllegalArgumentException("n is too big");
+
+            // Move the pointers ahead one
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
 }
--- src/test/java/com/joejernst/exercises/FibonacciTests.java
+++ src/test/java/com/joejernst/exercises/FibonacciTests.java
@@ -1,4 +1,44 @@
-package com.joejernst.excercises;
+package com.joejernst.exercises;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FibonacciTests {
+
+    @Test
+    public void testFirstTwoPositions() {
+        assertEquals(0, Fibonacci.findNthNumber(0));
+        assertEquals(1, Fibonacci.findNthNumber(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeNumber() {
+        Fibonacci.findNthNumber(-1);
+    }
+
+    @Test
+    public void testKnownGoodValues() {
+        assertEquals(2, Fibonacci.findNthNumber(3));
+        assertEquals(3, Fibonacci.findNthNumber(4));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReallyBigN() {
+        long result = Fibonacci.findNthNumber(10000);
+        System.out.println(result);
+
+        assertTrue(result > 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLowerLimit() {
+        Fibonacci.findNthNumber(Integer.MIN_VALUE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpperLimit() {
+        Fibonacci.findNthNumber(Integer.MAX_VALUE);
+    }
 }
